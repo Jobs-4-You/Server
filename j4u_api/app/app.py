d2e14236091c40d8fc36cb7a0b9d720cc38ca2cb
JimZer <jimi.vaubien@protonmail.com>
2020-05-21 23:14:33 +0200

    .

--- j4u_api/app/app.py
+++ j4u_api/app/app.py
@@ -2,8 +2,9 @@ import glob
 import os
 import uuid
 from datetime import date
+from io import BytesIO
 
-from flask import Flask, render_template, request, send_file
+from flask import Flask, g, render_template, request, send_file
 from flask_cors import CORS, cross_origin
 from flask_graphql import GraphQLView
 from weasyprint import HTML
@@ -50,42 +51,43 @@ app.add_url_rule(
 @app.route("/certificate", methods=["POST"])
 @cross_origin()  # allow all origins all methods.
 def certificate():
-    try:
-        content = request.get_json()
-        templateData = {
-            "civilite": content["civilite"],
-            "jobTitle": content["jobTitle"],
-            "firstName": content["firstName"],
-            "lastName": content["lastName"],
-            "birthDate": content["birthDate"],
-            "server": config.APP_DOCKER_URL,
-            "today": date.today(),
-            "timestamp": "timestamp",
-        }
-        certificate = render_template("certificate.html", **templateData)
-        uuid_name = f"{str(uuid.uuid4())}.pdf"
-        pdf_temp_path = os.path.join(config.CERT_TEMP_PATH, uuid_name)
-        HTML(string=certificate).write_pdf(pdf_temp_path)
-        return send_file(pdf_temp_path, as_attachment=True, mimetype="application/pdf")
-    except Exception as err:
-        print(err)
-        print(err)
-        print(err)
-        print(err)
-        print(err)
+    content = request.get_json()
+    templateData = {
+        "civilite": content["civilite"],
+        "jobTitle": content["jobTitle"],
+        "firstName": content["firstName"],
+        "lastName": content["lastName"],
+        "birthDate": content["birthDate"],
+        "server": config.APP_DOCKER_URL,
+        "today": date.today(),
+        "timestamp": "timestamp",
+    }
+    certificate = render_template("certificate.html", **templateData)
+    uuid_name = f"{str(uuid.uuid4())}.pdf"
+    pdf_temp_path = os.path.join(config.CERT_TEMP_PATH, uuid_name)
+    HTML(string=certificate).write_pdf(pdf_temp_path)
+    with open(pdf_temp_path, "rb") as fh:
+        buf = BytesIO(fh.read())
+    os.remove(pdf_temp_path)
+    return send_file(
+        buf,
+        as_attachment=True,
+        mimetype="application/pdf",
+        attachment_filename="certificate.pdf",
+    )
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
     trash_pdfs = glob.glob(f"{config.CERT_TEMP_PATH}/*.pdf")
-    for pdf in trash_pdfs:
-        os.remove(pdf)
-    n_removed = len(trash_pdfs)
-    logger.info(
-        __name__,
-        shutdown_session,
-        "%d temp certificate PDFs removed",
-        n_removed,
-        extra={"cert_temp_pdfs_removed": n_removed},
-    )
+    # for pdf in trash_pdfs:
+    #    os.remove(pdf)
+    # n_removed = len(trash_pdfs)
+    # logger.info(
+    #    __name__,
+    #    shutdown_session,
+    #    "%d temp certificate PDFs removed",
+    #    n_removed,
+    #    extra={"cert_temp_pdfs_removed": n_removed},
+    # )
